a248ceb11942a8e920007bd13843d0f1323e52ac
Naludrag <stephanecarvalho81@gmail.com>
2020-04-01 14:52:56 +0200

    Modified tests of Die and Board to use BeforeEach

--- src/test/java/ch/heigvd/gen/monopoly/board/BoardTest.java
+++ src/test/java/ch/heigvd/gen/monopoly/board/BoardTest.java
@@ -1,20 +1,25 @@
 package ch.heigvd.gen.monopoly.board;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BoardTest {
+    private Board board;
+
+    @BeforeEach
+    public void createBoard(){
+        board = new Board();
+    }
 
     @Test
-    public void testSquareNameGo() {
-        Board board = new Board();
+    public void testSquareNameGo(){
         Square square = board.getSquare(0);
         assertEquals("Go", square.getName());
     }
 
     @Test
     public void testSquaresName() {
-        Board board = new Board();
         for(int i = 1; i < 40; i++){
             String str = "Square " + i;
             assertEquals(str,  board.getSquare(i).getName());
@@ -24,7 +29,6 @@ public class BoardTest {
     @Test
     public void testGoToDesiredSquare() {
         Square go = new Square("Go");
-        Board board = new Board();
         assertEquals("Square 2",board.getSquare(go, 2).getName());
         assertEquals("Square 10",board.getSquare(go, 10).getName());
     }
@@ -32,7 +36,6 @@ public class BoardTest {
     @Test
     public void testFaceNotInLimits() {
         Square go = new Square("Go");
-        Board board = new Board();
         assertNull(board.getSquare(go, 1));
         assertNull(board.getSquare(go, 13));
     }
@@ -40,14 +43,12 @@ public class BoardTest {
     @Test
     public void testSquareNull() {
         Square go = null;
-        Board board = new Board();
         assertNull(board.getSquare(go, 2));
     }
 
     @Test
     public void testSquareNotInBoard() {
         Square go = new Square("SquareUnknown");
-        Board board = new Board();
         assertNull(board.getSquare(go, 2));
     }
 }
--- src/test/java/ch/heigvd/gen/monopoly/pieces/DieTest.java
+++ src/test/java/ch/heigvd/gen/monopoly/pieces/DieTest.java
@@ -1,19 +1,26 @@
 package ch.heigvd.gen.monopoly.pieces;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DieTest {
+    private Die die;
+
+    @BeforeEach
+    public void createDie(){
+        die = new Die();
+    }
+
     @Test
     public void testFaceValueDefault() {
-        Die die = new Die();
         assertEquals(1, die.getFaceValue());
     }
 
     @RepeatedTest(10)
     public void testRoll() {
-        Die die = new Die();
         die.roll();
         assertTrue(die.getFaceValue() <= 6, "Error roll is too high");
         assertTrue(die.getFaceValue() >= 1, "Error roll is too low");
