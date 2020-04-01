491365ac38d4e3e540bfe82f9864bb54369e6dcf
Robin Müller <33422394+AziXus@users.noreply.github.com>
2020-04-01 15:52:03 +0200

    Added cash to player and tests

--- src/main/java/ch/heigvd/gen/monopoly/Player.java
+++ src/main/java/ch/heigvd/gen/monopoly/Player.java
@@ -1,21 +1,25 @@
 package ch.heigvd.gen.monopoly;
 
-import ch.heigvd.gen.monopoly.board.Square;
+import ch.heigvd.gen.monopoly.board.square.Square;
 import ch.heigvd.gen.monopoly.pieces.Die;
 import ch.heigvd.gen.monopoly.pieces.Piece;
 import ch.heigvd.gen.monopoly.board.Board;
 
 public class Player {
+    private final static int INITIAL_CASH = 1500;
+
     private String name;
     private Piece piece;
     private Board board;
     private Die[] dice;
+    private int cash;
 
     public Player(String name, Square go, Board board, Die[] dice) {
         this.name = name;
         this.piece = new Piece(go);
         this.board = board;
         this.dice = dice;
+        this.cash = INITIAL_CASH;
     }
 
     public void takeTurn() {
@@ -29,5 +33,23 @@ public class Player {
         Square oldLoc = piece.getLocation();
         Square newLoc = board.getSquare(oldLoc, fvTot);
         piece.setLocation(newLoc);
+    }
+
+    public int getCash() {
+        return cash;
+    }
+
+    public void addCash(int cashToAdd) {
+        if (cashToAdd <= 0)
+            throw new IllegalArgumentException("cashToAdd must be a positive higher than 0 integer");
+
+        this.cash += cashToAdd;
+    }
+
+    public void removeCash(int cashToRemove) {
+        if (cashToRemove <= 0)
+            throw new IllegalArgumentException("cashToRemove must be a positive higher than 0 integer");
+
+        this.cash -= cashToRemove;
     }
 }
