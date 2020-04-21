7e006eebc5285715cadc386bfd5e677ee1b767e6
Robin Müller <33422394+AziXus@users.noreply.github.com>
2020-04-21 11:34:59 +0200

    Replaced 200 by static final attribute CASH_INCOME

--- src/main/java/ch/heigvd/gen/monopoly/board/square/GoSquare.java
+++ src/main/java/ch/heigvd/gen/monopoly/board/square/GoSquare.java
@@ -3,12 +3,14 @@ package ch.heigvd.gen.monopoly.board.square;
 import ch.heigvd.gen.monopoly.Player;
 
 public class GoSquare extends Square {
+    private static final int CASH_INCOME = 200;
+
     public GoSquare() {
         super("Go");
     }
 
     @Override
     public void landedOn(Player p) {
-        p.addCash(200);
+        p.addCash(CASH_INCOME);
     }
 }
