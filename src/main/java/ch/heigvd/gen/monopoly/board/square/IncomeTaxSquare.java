f53d6567bfa1c3165e10da2a13437863235fcbbe
Robin Müller <33422394+AziXus@users.noreply.github.com>
2020-04-22 13:30:46 +0200

    Comments added

--- src/main/java/ch/heigvd/gen/monopoly/board/square/IncomeTaxSquare.java
+++ src/main/java/ch/heigvd/gen/monopoly/board/square/IncomeTaxSquare.java
@@ -2,11 +2,22 @@ package ch.heigvd.gen.monopoly.board.square;
 
 import ch.heigvd.gen.monopoly.Player;
 
+/**
+ * Representation of the income tax square for a Monopoly game
+ */
 public class IncomeTaxSquare extends Square {
+    /**
+    * Default constructor of the IncomeTaxSquare that sets the square name to "IncomeTax"
+    */
     public IncomeTaxSquare() {
         super("IncomeTax");
     }
 
+    /**
+     * Override of the landedOn function that removes the minimum between 200 and 10% of
+     * the player net worth from his cash
+     * @param p player that landed on this square
+     */
     @Override
     public void landedOn(Player p) {
         int w = p.getNetWorth();
